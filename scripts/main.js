8170a218c622a873c08aca1fee0fa214eeb5ad70
Changbai Li <lcb931023@gmail.com>
2021-02-06 14:41:58 +0800

    Smart backing track added

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,50 @@
 const TRACKER_LENGTH = 64;
+
+const backingTrack = new Tone.Player("audio/backing/so-what-32m.mp3").toDestination();
+backingTrack.volume.value = -9
+const backingLoop = new Tone.Loop(time => {
+  console.log("playing backing track");
+  backingTrack.start();
+}, "32m").start(0);
+
 // sequencer keeps time and triggers playing each beat
 const sequencer = new Tone.Sequence(
   onBeat,
   new Array(TRACKER_LENGTH).fill().map((x,i) => i), // [0,1,2,3,4,5...
   "16n").start(0);
 
+let beatCounter = 0
+
 function onBeat (time, index) {
-  // interpret lines in the tracker
   // TODO handle situation when the tracker has more than 64 beats
+  
+  beatCounter ++; // the number of 16n notes played so far. Starts with 1
+  // interpret lines in the tracker
   const note = tracker[index]
   if (!note) return
   if (!note[0]) return
+  let pitch = note[0]
+  const duration = note[1]
+  // emphasize odd 8th notes
   const velocity = (index % 4 == 2 || index % 4 == 3) ? 0.6:1
-  piano.triggerAttackRelease(note[0], note[1], time, velocity)
+  // shift the pitch for that 8 measures of E-11 chord
+  // TODO algorify magic math
+  const measurePosition = beatCounter % (16*32)
+  if (measurePosition > (16*16) && measurePosition <= (16*24)) {
+    pitch = Tone.Frequency(pitch).transpose(1);
+  }
+  piano.triggerAttackRelease(pitch, duration, time, velocity)
 };
 
 
-// Tone.Transport.bpm.value = 120;
+Tone.Transport.bpm.value = 140;
 Tone.Transport.swing = .3; // this actually can't be too high. 0.3 is a good triplet feel
 Tone.Transport.swingSubdivision = "8n";
-Tone.Transport.start();
+Tone.loaded().then(() => {
+  console.log("loaded!");
+  Tone.Transport.start();
+})
+
 
 // tracker keeps the musical content.
 // spaces are added using null
@@ -31,6 +56,11 @@ let tracker = []
  */
 function addLine(line) {
   tracker = tracker.concat(convertLine(line))
+  // There's a chance that we add a rest
+  if (Math.random() < 0.5) {
+    const rest = Rests[ Math.floor(Math.random() * Rests.length) ]
+    tracker = tracker.concat(convertLine(rest))
+  }
 }
 
 /**
